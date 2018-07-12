700ac9213c63e9d485308e1afb2e45c0a477d1c5
Jing Luo <jamyluo.hust@gmail.com>
2018-07-12 11:38:15 +0800

    Add security group id output.

--- modules/consul-cluster/outputs.tf
+++ modules/consul-cluster/outputs.tf
@@ -5,3 +5,7 @@ output "ess_name" {
 output "cluster_size" {
   value = "${alicloud_ess_scaling_group.autoscaling_group.min_size}"
 }
+
+output "security_groupd_id" {
+  value = "${alicloud_security_group.security_group.id}"
+}
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "vpc_id" {
   value = "${data.alicloud_vpcs.default.id}"
 }
+
+output "security_groupd_id" {
+  value = "${module.consul_servers.security_group_id}"
+}
